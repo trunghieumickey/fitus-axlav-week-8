31d2cef0dd90984ba5813ac299732203d84ecfe6
trunghieumickey <trunghieumickey@outlook.com.vn>
2023-04-05 18:36:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 // load face-api.js models
-const modelLink = "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights"
-
-Promise.all([
-    faceapi.nets.tinyYolov2.loadFromUri(modelLink),
-    // faceapi.nets.faceLandmark68Net.loadFromUri(modelLink),
-    // faceapi.nets.faceRecognitionNet.loadFromUri(modelLink),
-    // faceapi.nets.faceExpressionNet.loadFromUri(modelLink)
-  ]).then(startVideo)
-
 import * as faceapi from 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api';
 
 // get video element and canvas element
+const modelLink = "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights"
 const video = document.getElementById('video')
 const canvas = document.getElementById('canvas')
 
-// start video stream
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
-}
+// load the face detection model
+await faceapi.nets.tinyFaceDetector.loadFromUri(modelLink);
 
-// detect faces and draw bounding boxes on canvas
-video.addEventListener('play', () => {
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-  }, 100)
-})
+// start the video stream
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => video.srcObject = stream)
+  .catch(err => console.error(err));
+
+// detect faces and draw bounding boxes every 100 ms
+setInterval(async () => {
+  // get the face detections from the video element
+  const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
+
+  // resize the canvas to match the video size
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  // clear the previous canvas content
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+  // draw the bounding boxes on the canvas
+  const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  faceapi.draw.drawDetections(canvas, resizedDetections);
+}, 100);
