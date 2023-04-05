c4d852903c32856fd43b57ed9efc9201184615c2
trunghieumickey <trunghieumickey@outlook.com.vn>
2023-04-05 17:50:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Promise.all([
     faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
-import * as faceapi from 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/dist/face-api.min.js';
+import * as faceapi from 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api';
 
 // get canvas element
 const canvas = document.getElementById('root')
