de552a5060bfa02d9cf3d65e3ed963ed3c074186
trunghieumickey <trunghieumickey@outlook.com.vn>
2023-04-05 18:16:46 +0700

    Updates

--- index.js
+++ index.js
@@ -10,38 +10,27 @@ Promise.all([
 
 import * as faceapi from 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api';
 
-// get canvas element
-const canvas = document.getElementById('root')
-
-// create video element from URL
-const videoURL = 'https://raw.githubusercontent.com/tensorflow/tfjs-models/master/pose-detection/test_data/pose_squats.mp4'
-const video = document.createElement('video')
-video.src = videoURL
-video.crossOrigin = 'anonymous'
-video.loop = true
+// get video element and canvas element
+const video = document.getElementById('video')
+const canvas = document.getElementById('canvas')
 
 // start video stream
 function startVideo() {
-    video.addEventListener('loadeddata', function (e) {
-        // set video and canvas size
-        const displaySize = { width: video.videoWidth, height: video.videoHeight }
-        video.width = displaySize.width
-        video.height = displaySize.height
-        canvas.width = displaySize.width
-        canvas.height = displaySize.height
-
-        // append video to document body
-        document.body.appendChild(video)
-
-        // detect faces and draw bounding boxes on canvas
-        setInterval(async () => {
-            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-            const resizedDetections = faceapi.resizeResults(detections, displaySize)
-            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-            faceapi.draw.drawDetections(canvas, resizedDetections)
-        }, 100)
-
-        // play video
-        video.play()
-    })
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
 }
+
+// detect faces and draw bounding boxes on canvas
+video.addEventListener('play', () => {
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+  }, 100)
+})
